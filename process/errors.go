7eacd804753b90ba54359f985f889f448fb224f6
Marius C <marius.ciucam@gmail.com>
2023-05-29 12:12:22 +0300

    FIX: Linter

--- process/errors.go
+++ process/errors.go
@@ -2,8 +2,6 @@ package process
 
 import "errors"
 
-var errInvalidOperationType = errors.New("invalid/unknown operation type")
-
 var errNilMarshaller = errors.New("nil marshaller provided")
 
 var errNilOutportBlockData = errors.New("nil outport block data")
