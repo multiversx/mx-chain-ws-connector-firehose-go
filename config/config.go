444df1451216aa0cc4c288c19e01e1c87b682205
Adrian Dobrita <anaridado@gmail.com>
2024-02-09 17:18:56 +0200

    update fields

--- config/config.go
+++ config/config.go
@@ -12,7 +12,8 @@ type WebSocketConfig struct {
 	Mode                       string `toml:"mode"`
 	RetryDuration              uint32 `toml:"retry_duration"`
 	WithAcknowledge            bool   `toml:"with_acknowledge"`
+	AcknowledgeTimeoutInSec    int    `toml:"acknowledge_timeout_in_sec"`
 	BlockingAckOnError         bool   `toml:"blocking_ack_on_error"`
-	DropMessagesIfNoConnection bool   // Set to `true` to drop messages if there is no active WebSocket connection to send to.
-	Version                    uint32 // Defines the payload version.
+	DropMessagesIfNoConnection bool   `toml:"drop_messages_if_no_connection"` // Set to `true` to drop messages if there is no active WebSocket connection to send to.
+	Version                    uint32 `toml:"version"`                        // Defines the payload version.
 }
