2e28e6101ca70dad570f8271d7c312b5c84fdcea
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-04-11 16:21:13 +0300

    added proto marshalizer and tests.

--- process/protoMarshalizer.go
+++ process/protoMarshalizer.go
@@ -1,22 +1,29 @@
 package process
 
 import (
+	"fmt"
+
 	"google.golang.org/protobuf/proto"
 )
 
 type ProtoMarshaller struct{}
 
-func (p ProtoMarshaller) Marshal(obj interface{}) ([]byte, error) {
-
-	return proto.Marshal(obj)
+func (p *ProtoMarshaller) Marshal(obj interface{}) ([]byte, error) {
+	message, ok := obj.(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("cannot marshal %T into a proto.Message", obj)
+	}
+	return proto.Marshal(message)
 }
 
-func (p ProtoMarshaller) Unmarshal(obj interface{}, buff []byte) error {
-	//TODO implement me
-	panic("implement me")
+func (p *ProtoMarshaller) Unmarshal(obj interface{}, buff []byte) error {
+	message, ok := obj.(proto.Message)
+	if !ok {
+		return fmt.Errorf("cannot marshal %T into a proto.Message", obj)
+	}
+	return proto.Unmarshal(buff, message)
 }
 
-func (p ProtoMarshaller) IsInterfaceNil() bool {
-	//TODO implement me
-	panic("implement me")
+func (p *ProtoMarshaller) IsInterfaceNil() bool {
+	return p == nil
 }
---
+++ process/protoMarshalizer_test.go
@@ -0,0 +1,78 @@
+package process
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/multiversx/mx-chain-ws-connector-template-go/data"
+)
+
+func recovedMarshal(obj interface{}) (buf []byte, err error) {
+	protoMarshaller := ProtoMarshaller{}
+
+	defer func() {
+		if p := recover(); p != nil {
+			if panicError, ok := p.(error); ok {
+				err = panicError
+			} else {
+				err = fmt.Errorf("%#v", p)
+			}
+			buf = nil
+		}
+	}()
+	buf, err = protoMarshaller.Marshal(obj)
+	return
+}
+
+func recovedUnmarshal(obj interface{}, buf []byte) (err error) {
+	protoMarshaller := ProtoMarshaller{}
+
+	defer func() {
+		if p := recover(); p != nil {
+			if panicError, ok := p.(error); ok {
+				err = panicError
+			} else {
+				err = fmt.Errorf("%#v", p)
+			}
+		}
+	}()
+	err = protoMarshaller.Unmarshal(obj, buf)
+	return
+}
+
+func TestGogoProtoMarshalizer_Marshal(t *testing.T) {
+	outportBlock := data.ShardOutportBlock{}
+	encNode, err := recovedMarshal(&outportBlock)
+	assert.Nil(t, err)
+	assert.NotNil(t, encNode)
+}
+
+func TestGogoProtoMarshalizer_MarshalWrongObj(t *testing.T) {
+	obj := "multiversx"
+	encNode, err := recovedMarshal(obj)
+	assert.Nil(t, encNode)
+	assert.NotNil(t, err)
+}
+
+func TestGogoProtoMarshalizer_Unmarshal(t *testing.T) {
+	protoMarshaller := ProtoMarshaller{}
+	outportBlock := data.ShardOutportBlock{}
+
+	encNode, _ := protoMarshaller.Marshal(outportBlock)
+	newNode := &data.ShardOutportBlock{}
+
+	err := recovedUnmarshal(newNode, encNode)
+	assert.Nil(t, err)
+	assert.Equal(t, outportBlock.ProtoReflect(), newNode.ProtoReflect())
+}
+
+func TestGogoProtoMarshalizer_UnmarshalWrongObj(t *testing.T) {
+	protoMarshaller := ProtoMarshaller{}
+	outportBlock := data.ShardOutportBlock{}
+
+	encNode, _ := protoMarshaller.Marshal(outportBlock)
+	err := recovedUnmarshal([]byte{}, encNode)
+	assert.NotNil(t, err)
+}
