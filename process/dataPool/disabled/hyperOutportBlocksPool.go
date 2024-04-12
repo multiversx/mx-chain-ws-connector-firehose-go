f665e4d15aa40d86e27278c04f4634dcf284f3bf
ssd04 <dariussuirab@gmail.com>
2024-04-12 17:35:55 +0300

    added disabled blocks pool components

---
+++ process/dataPool/disabled/hyperOutportBlocksPool.go
@@ -0,0 +1,36 @@
+package disabled
+
+import (
+	"github.com/multiversx/mx-chain-ws-connector-template-go/data"
+)
+
+type disabledHyperOutportBlocksPool struct {
+}
+
+// NewDisabledHyperOutportBlocksPool will create a new disabled blocks pool component
+func NewDisabledHyperOutportBlocksPool() *disabledHyperOutportBlocksPool {
+	return &disabledHyperOutportBlocksPool{}
+}
+
+// UpdateMetaState does nothing
+func (bp *disabledHyperOutportBlocksPool) UpdateMetaState(round uint64) {
+}
+
+// PutBlock returns nil
+func (bp *disabledHyperOutportBlocksPool) PutBlock(hash []byte, outportBlock *data.HyperOutportBlock, currentRound uint64) error {
+	return nil
+}
+
+// GetBlock returns nil
+func (bp *disabledHyperOutportBlocksPool) GetBlock(hash []byte) (*data.HyperOutportBlock, error) {
+	return nil, nil
+}
+
+func (bp *disabledHyperOutportBlocksPool) Close() error {
+	return nil
+}
+
+// IsInterfaceNil returns nil if there is no value under the interface
+func (bp *disabledHyperOutportBlocksPool) IsInterfaceNil() bool {
+	return bp == nil
+}
