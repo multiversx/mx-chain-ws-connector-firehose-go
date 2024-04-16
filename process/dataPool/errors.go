e419464bc191ba374e8e1f4ee6a909ca84ae66ee
ssd04 <dariussuirab@gmail.com>
2024-04-16 11:45:28 +0300

    added failed to put block error

--- process/dataPool/errors.go
+++ process/dataPool/errors.go
@@ -7,3 +7,6 @@ var ErrNilDataPool = errors.New("nil data pool provided")
 
 // ErrNilMetaOutportBlock signals that a nil meta outport block was provided
 var ErrNilMetaOutportBlock = errors.New("nil meta outport block was provided")
+
+// ErrFailedToPutBlockDataToPool signals that it cannot put block data into pool
+var ErrFailedToPutBlockDataToPool = errors.New("failed to put block data")
