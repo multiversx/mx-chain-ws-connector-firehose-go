e24e93135beec1965c3b2a5b1df91dde63e1ea82
ssd04 <dariussuirab@gmail.com>
2024-04-24 09:58:34 +0300

    added db modes comments

--- common/common.go
+++ common/common.go
@@ -1,7 +1,14 @@
 package common
 
 const (
-	FullPersisterDBMode      = "full-persister"
-	ImportDBMode             = "import-db"
+	// FullPersisterDBMode defines the db mode where each event will be saved
+	// to cache and persister
+	FullPersisterDBMode = "full-persister"
+
+	// ImportDBMode defines the db mode where the events will be saved only to cache
+	ImportDBMode = "import-db"
+
+	// OptimizedPersisterDBMode defines the db mode where the events will be saved to
+	// cache, and they will be dumped to persister when necessary
 	OptimizedPersisterDBMode = "optimized-persister"
 )
